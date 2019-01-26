4e221e64d21e25bfcb553701c0c30d06124f4b8f
tangentmonger <tangentmonger@users.noreply.github.com>
2019-01-26 01:02:47 +0000

    Smooth fast smoke

--- analyse.py
+++ analyse.py
@@ -53,37 +53,29 @@ accumulator = np.zeros((accumulator_height, accumulator_width), dtype=np.uint8)
 y_idxs, x_idxs = np.nonzero(np.asarray(im))  # (row, col) indexes to ink pixels
 
 
-frames = 50
-frame_nth = int(num_thetas/frames)
-print(frame_nth)
-
 brightness = 3
 
 scanner = np.zeros((width, height), dtype=np.uint8)
 
 started_image = False
 
-# Vote in the hough accumulator
-with imageio.get_writer('output.gif', mode='I') as writer:
-  #for each column (as if it was being scanned in)
-  print(f"height, width = {height},{width}")
+def add(column, row):
+  accumulator[column, row] = min(accumulator[column, row] + brightness, 255)
 
+# Vote in the hough accumulator
+with imageio.get_writer('output.gif', mode='I', fps=50) as writer:
+  #for each column (as if it was being scanned in)
 
 
   for column in reversed(range(width)):
-    print(f"column {column}")
+    print(f"accumulator_index {accumulator_index}")
 
     image_data = np.asarray(im)
 
     column_pixels = image_data[:,column]
 
     x_idxs = np.nonzero(column_pixels)[0]  # (row, col) indexes to ink pixels
-    print(f"ink pixels in this column: {len(x_idxs)}")
-    print(x_idxs)
     started_image |= (len(x_idxs) > 0)
-    #
-    #
-    #
 
 
     for i in range(len(x_idxs)):  # for each ink pixel in the column
@@ -95,27 +87,19 @@ with imageio.get_writer('output.gif', mode='I') as writer:
         t_offset_idx = (t_idx + accumulator_index) % num_thetas
 
         # Calculate rho. diag_len is added for a positive index
-        #print(f"t_idx {t_idx}")
-        #print(f"t_actual_idx {t_actual_idx}")
-        #print(f"accumulator_end_row {accumulator_end_row}")
-
-        #rho = int(x * cos_t[t_offset_idx] + y * sin_t[t_offset_idx] + diag_len)
         rho = int(x * cos_t[t_offset_idx] + y * sin_t[t_offset_idx] + diag_len)
-        #accumulator[t_idx + accumulator_index + page_height, rho] += brightness
-
 
         if (t_idx + accumulator_index) % page_height +1 > half_page_height:
-          accumulator[t_idx + accumulator_index + page_height, (2 * diag_len) - 1 - rho] += brightness
-          accumulator[t_idx + accumulator_index + page_height + half_page_height, rho] += brightness
-          accumulator[t_idx + accumulator_index + page_height*2 , (2 * diag_len) - 1 - rho] += brightness
-          accumulator[t_idx + accumulator_index + page_height*2 + half_page_height, rho] += brightness
+          add(t_idx + accumulator_index + page_height, (2 * diag_len) - 1 - rho)
+          add(t_idx + accumulator_index + page_height + half_page_height, rho)
+          add(t_idx + accumulator_index + page_height*2 , (2 * diag_len) - 1 - rho)
+          #add(t_idx + accumulator_index + page_height*2 + half_page_height, rho)
 
         else:
-          accumulator[t_idx + accumulator_index + page_height, rho] += brightness
-          accumulator[t_idx + accumulator_index + page_height + half_page_height, (2 * diag_len) - 1 - rho] += brightness
-          accumulator[t_idx + accumulator_index + page_height*2, rho] += brightness
-          accumulator[
-            t_idx + accumulator_index + page_height*2 + half_page_height, (2 * diag_len) - 1 - rho] += brightness
+          add(t_idx + accumulator_index + page_height, rho)
+          add(t_idx + accumulator_index + page_height + half_page_height, (2 * diag_len) - 1 - rho)
+          add(t_idx + accumulator_index + page_height*2, rho)
+          #add(t_idx + accumulator_index + page_height*2 + half_page_height, (2 * diag_len) - 1 - rho)
 
     accumulator_index += 1
 
@@ -123,10 +107,11 @@ with imageio.get_writer('output.gif', mode='I') as writer:
     if started_image:
       writer.append_data(np.copy(accumulator[output_index:output_index+page_height, :]))
       output_index +=1
-      #writer.append_data(accumulator)
 
   for index in range(output_index, page_height*3):
+    print(f"index {index}")
     writer.append_data(np.copy(accumulator[index:index + page_height, :]))
+
 
 
 #output = Image.fromarray(accumulator)
